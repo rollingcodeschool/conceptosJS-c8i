0206588e8ff9fa91fb1c5d31aa8c9e5499dab04f
Emilse Arias <emilsearias@gmail.com>
2022-06-29 18:35:26 -0300

    creamos nuestras primeras variables

--- js/1-variables.js
+++ js/1-variables.js
@@ -4,7 +4,7 @@
 de multiples
 lineas */
 
-//mostrar un mensaje en el documento html o pantalla
+//mostrar o imprimir un mensaje en el documento html o pantalla
 document.write('<h1>Hola mundo, año 2022, canción</h1>');
 
 // mostrar un mensaje por consola
@@ -17,4 +17,19 @@ console.log("alguna frase");
 document.write('<p>Resultado: ' + (29 + 6) +'</p>');
 // Resultado: 296
 
-// Variables 
+// crear o declarar o inicializar Variables (var - let - const)
+
+let nombrePersona = 'Cristian';
+let dolar = 238;
+
+document.write('<p>Persona: ' + nombrePersona + '</p>');
+document.write('<p>Play5: $'+(500 * dolar) + ' sin impuestos. <p>')
+
+// actualizar el valor del dolar
+dolar = 220; 
+document.write('<p>Play5: $'+(500 * dolar) + ' sin impuestos. <p>');
+
+// pedimos al usuario que ingrese el valor actual del dolar
+dolar = parseInt(prompt('Ingrese el valor del dolar'));
+console.log(dolar);
+document.write('<p>Play5: $'+(500 * dolar) + ' sin impuestos. <p>');
