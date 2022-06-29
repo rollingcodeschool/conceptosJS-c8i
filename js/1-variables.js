dde85dd6e8f6cc8035ba38096ea53b5f3ee00639
Emilse Arias <emilsearias@gmail.com>
2022-06-29 18:07:38 -0300

    mostramos diferentes mensajes por pantalla, consola y alert

---
+++ js/1-variables.js
@@ -0,0 +1,20 @@
+//Esto es un comentario de una unica linea
+
+/* Esto es un comentario
+de multiples
+lineas */
+
+//mostrar un mensaje en el documento html o pantalla
+document.write('<h1>Hola mundo, año 2022, canción</h1>');
+
+// mostrar un mensaje por consola
+console.log("alguna frase");
+
+// mostrar un mensaje por una ventana emergente
+// alert('esto es un ventana emergente');
+
+// mostrar por pantalla la suma de dos numeros 29 + 6
+document.write('<p>Resultado: ' + (29 + 6) +'</p>');
+// Resultado: 296
+
+// Variables 
