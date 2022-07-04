a190040a1c6289ef827e1ba1ce7e3a33e3b1c2ef
Emilse Arias <emilsearias@gmail.com>
2022-07-04 18:34:50 -0300

    agregamos una segunda opcion con === para el ejemplo de condicionales

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -8,19 +8,30 @@ if(condicion logica){
 
 */
 
-let numero1 = parseInt(prompt('Ingrese un primer numero'));
-let numero2 = parseInt(prompt('Ingrese un segundo numero'));
+let numero1 = parseInt(prompt("Ingrese un primer numero"));
+let numero2 = parseInt(prompt("Ingrese un segundo numero"));
 
 console.log(numero1);
 console.log(numero2);
 
-if( numero1 > numero2){
-    document.write('El primer numero es el mayor, numero ingresado: '+ numero1);
-}else{
-    if(numero2 > numero1){
-        document.write('El segundo numero es el mayor, numero ingresado: '+ numero2); 
-    }else{
-        document.write('Los numeros ingresados son iguales, numero ingresado: '+ numero2); 
-    } 
-}
+// if (numero1 > numero2) {
+//   document.write("El primer numero es el mayor, numero ingresado: " + numero1);
+// } else if (numero2 > numero1) {
+//   document.write("El segundo numero es el mayor, numero ingresado: " + numero2);
+// } else {
+//   document.write(
+//     "Los numeros ingresados son iguales, numero ingresado: " + numero2
+//   );
 
+// hola mundo
+// }
+
+if (numero1 > numero2) {
+  document.write("El primer numero es el mayor, numero ingresado: " + numero1);
+} else if (numero2 === numero1) {
+    document.write(
+      "Los numeros ingresados son iguales, numero ingresado: " + numero2
+    );
+} else {
+    document.write("El segundo numero es el mayor, numero ingresado: " + numero2);
+}
