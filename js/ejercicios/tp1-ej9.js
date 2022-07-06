dce0d28e4444a46748071b29c9707dfccc4fc906
Emilse Arias <emilsearias@gmail.com>
2022-07-06 17:44:40 -0300

    se agrego el codigo del ejericio 9 del tp1

---
+++ js/ejercicios/tp1-ej9.js
@@ -0,0 +1,31 @@
+let frase = prompt('Ingrese una frase de hasta 7 caracteres');
+console.log(frase);
+console.log(frase.length)
+//cambiar toda la frase a minusculas
+console.log(frase.toLowerCase());
+//actualizar el valor de una variable
+frase = frase.toLowerCase();
+//hola mundo
+
+if(frase.charAt(0) === 'a' || frase.charAt(0) === 'e' || frase.charAt(0) === 'i' || frase.charAt(0) === 'o' || frase.charAt(0) === 'u'){
+    document.write(frase.charAt(0))
+}
+
+if(frase.charAt(1) === 'a' || frase.charAt(1) === 'e' || frase.charAt(1) === 'i' || frase.charAt(1) === 'o' || frase.charAt(1) === 'u'){
+    document.write(frase.charAt(1))
+}
+if(frase.charAt(2) === 'a' || frase.charAt(2) === 'e' || frase.charAt(2) === 'i' || frase.charAt(2) === 'o' || frase.charAt(2) === 'u'){
+    document.write(frase.charAt(2))
+}
+if(frase.charAt(3) === 'a' || frase.charAt(3) === 'e' || frase.charAt(3) === 'i' || frase.charAt(3) === 'o' || frase.charAt(3) === 'u'){
+    document.write(frase.charAt(3))
+}
+if(frase.charAt(4) === 'a' || frase.charAt(4) === 'e' || frase.charAt(4) === 'i' || frase.charAt(4) === 'o' || frase.charAt(4) === 'u'){
+    document.write(frase.charAt(4))
+}
+if(frase.charAt(5) === 'a' || frase.charAt(5) === 'e' || frase.charAt(5) === 'i' || frase.charAt(5) === 'o' || frase.charAt(5) === 'u'){
+    document.write(frase.charAt(5))
+}
+if(frase.charAt(6) === 'a' || frase.charAt(6) === 'e' || frase.charAt(6) === 'i' || frase.charAt(6) === 'o' || frase.charAt(6) === 'u'){
+    document.write(frase.charAt(6))
+}
