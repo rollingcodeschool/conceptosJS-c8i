ce529e0ea7a2eaae45f7d6d1adb467f7972d5ee8
Emilse Arias <emilsearias@gmail.com>
2022-07-06 18:34:15 -0300

    se agrego un ejemplo de bucle while

---
+++ js/3-bucles.js
@@ -0,0 +1,13 @@
+/*
+while(condicion logica){
+    todo el codigo que quiero ejecutar repetidas veces
+    cambiar la condicion logica
+}
+*/
+
+let cantidadMates = 20;
+
+while (cantidadMates >= 1) {
+  document.write("<br>Mate cebado N: " + cantidadMates);
+  cantidadMates--; // cantidadMates = cantidadMates - 1;
+}
