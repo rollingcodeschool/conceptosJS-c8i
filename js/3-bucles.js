97f4d371e0ca466f23f4bd0a8fca962fb4f5a29c
Emilse Arias <emilsearias@gmail.com>
2022-07-11 17:21:56 -0300

    agregamos el codigo de un bucle do-while

--- js/3-bucles.js
+++ js/3-bucles.js
@@ -1,13 +1,31 @@
+// /*
+// while(condicion logica){
+//     todo el codigo que quiero ejecutar repetidas veces
+//     cambiar la condicion logica
+// }
+// */
+
+// Ejemplo de while
+// let cantidadMates = 20;
+
+// while (cantidadMates >= 1) {
+//   document.write("<br>Mate cebado N: " + cantidadMates);
+//   cantidadMates--; // cantidadMates = cantidadMates - 1;
+// }
+
+// Ejemplo de bucle do-while, hacer ... mientras (se cumple la condicion logica)
+
 /*
-while(condicion logica){
-    todo el codigo que quiero ejecutar repetidas veces
+  do{
+     todas las lineas de codigo que quiero ejecutar varias veces
     cambiar la condicion logica
-}
+  }while(condicion logica)
 */
 
-let cantidadMates = 20;
 
-while (cantidadMates >= 1) {
+let cantidadMates = 0;
+
+do{
   document.write("<br>Mate cebado N: " + cantidadMates);
-  cantidadMates--; // cantidadMates = cantidadMates - 1;
-}
+  cantidadMates--; //cantidadMates = cantidadMates - 1;
+}while( cantidadMates >= 1)
