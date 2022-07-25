e090ee3ebd1e6b8fd9fc3d2ee57efcd1c5c63b9f
Emilse Arias <emilsearias@gmail.com>
2022-07-25 17:23:38 -0300

    agregamos el dibujo de la tabla con el calculo de la frecuencia

--- js/ejercicios/tp3-ej3.js
+++ js/ejercicios/tp3-ej3.js
@@ -20,3 +20,28 @@ for(let i = 0; i < 50; i++){
 document.write('Resultados dado1:'+ pruebaDado1);
 document.write('<br>Resultados dado2:'+ pruebaDado2);
 document.write('<br>Resultados sumas:'+ sumas);
+
+document.write(`<table class="tablaDatos">
+<thead>
+    <tr>
+        <th>Resultados</th>
+        <th>Frecuencia</th>
+    </tr>
+</thead>
+<tbody>`);
+//dibujar todas las filas que necesitamos
+for(let i = 2; i <= 12; i++){
+    document.write(`<tr>`);
+    //calcular la frecuencia de apariciones de mi variable i
+    let contador = 0;
+    for(let posicion = 0; posicion < sumas.length; posicion++ ){
+        if( i === sumas[posicion]){
+            //entonces quiero contar ese valor
+            contador++;
+        }
+    }
+    document.write(`<td>${i}</td><td>${contador}</td></tr>`);
+}
+
+document.write(`</tbody>
+</table>`);
