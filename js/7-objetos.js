7916cd5ffa576518f899d5655d4912a5b0d679a4
Emilse Arias <emilsearias@gmail.com>
2022-07-27 16:56:03 -0300

    agregamos, modificamos y eliminamos propiedades de un objeto con notacion literal

--- js/7-objetos.js
+++ js/7-objetos.js
@@ -19,4 +19,20 @@ const pelicula = {
     pausar: () => { document.write('<p>Se pauso la peli</p>')}
 };
 
-console.log(pelicula)
+// mostrar el objeto
+console.log(pelicula);
+document.write(`<p>Titulo: ${pelicula.titulo}</p>`);
+document.write(`<p>Duración: ${pelicula['duracion'] }</p>`);
+
+//modificar una propiedad del objeto
+pelicula.duracion = '3:00';
+document.write(`<p>Duración: ${pelicula['duracion'] }</p>`);
+
+//agregar una nueva propiedad a un objeto existente
+pelicula.anioPublicacion = 2022;
+document.write(`<p>Año de publicación: ${pelicula.anioPublicacion}</p>`);
+
+//borrar una propiedad de un objeto
+delete pelicula.anioPublicacion;
+document.write(`<p>Año de publicación: ${pelicula.anioPublicacion}</p>`);
+console.log(pelicula);
