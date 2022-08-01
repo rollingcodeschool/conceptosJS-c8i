855b981cf2076e86a1985112be3ba55cac59b9a5
Emilse Arias <emilsearias@gmail.com>
2022-08-01 18:39:03 -0300

    agregamos los get, set a la clase usuario y luego creamos la clase alumno heredando de usuario

--- js/8-clases.js
+++ js/8-clases.js
@@ -1,5 +1,6 @@
 // Usuario: nombre, apellido, password, email, fechaNacimiento, estado.
 
+
 class Usuario{
     //primer metodo que se ejecuta cuando creamos objetos usuarios
     constructor(nombrePersona, apellido, password, email, fechaNacimiento){
@@ -29,7 +30,69 @@ class Usuario{
         </ul>`)
     }
 
+    //propiedades conmutadas get y set
+    set modificarNombre(nuevoNombre){
+        this.nombre = nuevoNombre;
+    }
+
+    get mostrarNombre(){
+        return this.nombre;
+    }
+
 } 
+
+//Herencia
+// Alumno: curso, comision, estadoArancelario= true, calificaciones=[] , asistencia = 0.
+class Alumno extends Usuario{
+    constructor(curso, comision, _apellido, _nombrePersona , _password, _email, fechaNacimiento ){
+        //invocar al constructor de la clase padre
+        super(_nombrePersona, _apellido, _password,_email, fechaNacimiento);
+        this.curso = curso;
+        this.comision = comision;
+        this.estadoArancelario = true;
+        this.calificaciones = [];
+        this.asistencia = 0;
+    }
+
+    calificar(nota){
+        this.calificaciones.push(nota);
+    }
+    
+    obtenerPromedio(){
+        let cantidadNotas = this.calificaciones.length;
+        let sumaNotas = 0
+        for(let i = 0; i < cantidadNotas; i++){
+            sumaNotas += this.calificaciones[i]; // sumaNotas = sumaNotas + this.calificaciones[i]
+        }
+        let promedio = sumaNotas / cantidadNotas;
+        document.write(`<p>El promedio de ${this.nombre}, ${this.apellido} es ${promedio}</p>`)
+    }
+
+    cambiarComision(){}
+
+    mostrarDatos(){
+        document.write(`
+        <h3>Datos Personales</h3>
+        <hr>
+        <ul>
+            <li>Nombre: ${this.nombre}</li>
+            <li>Apellido: ${this.apellido} </li>
+        </ul>
+        <h3>Datos Academicos</h3>
+        <hr>
+        <ul>
+            <li>Curso:${this.curso}</li>
+            <li>Comision: ${this.comision}</li>
+            <li>Calificaciones: ${this.calificaciones}</li>
+        </ul>
+        `)
+    }
+
+    //agregar los get y setNecesarios.
+
+}
+
+
 
 // crear un objeto o instanciar
 let vicky = new Usuario('Victoria','Palacio Fernández', 'Sfgkdjsd234%','vicky@gmail.com','06/1997');
@@ -38,3 +101,23 @@ let gabriel = new Usuario('Gabriel','Azubel', 'sddddkdjsd234%','Gabriel@gmail.co
 console.log(vicky);
 vicky.mostrarDatos();
 gabriel.mostrarDatos();
+
+//usar propiedades conmutadas
+gabriel.modificarNombre = 'Cristian Gabriel';
+gabriel.mostrarDatos();
+
+document.write(`<p>Usuario modificado ${gabriel.mostrarNombre}</p>`) 
+
+//creo un objeto alumno
+let agustin = new Alumno('FullStack', 'c8i', 'Sampayo','Agustin','sjdfhA3746$', 'agustin@gmail.com', '06/1997');
+
+agustin.mostrarDatos();
+
+agustin.calificar(9);
+agustin.calificar(8);
+agustin.calificar(10);
+
+agustin.obtenerPromedio();
+console.log(agustin);
+
+agustin.mostrarDatos();
